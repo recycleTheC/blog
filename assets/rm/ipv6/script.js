c207419940573bc72708e63e4b33a8572c62d01e
recycleTheC <mario.kopjar1@gmail.com>
2020-01-29 18:17:41 +0100

    Update & debug

--- assets/rm/ipv6/script.js
+++ assets/rm/ipv6/script.js
@@ -1,61 +1,87 @@
+/**********************************************************************
+ * IPv6 Address Compression Tool                                      *
+ **********************************************************************/
+/**
+ * Copyright (c) 2020. Mario Kopjar
+ */
+
+/**
+ * Ispituje valjanost unesene IPv6 adrese i vraća rezultat ispitivanja.
+ * @return {bool} valjana/ne valjana IPv6 adresa
+ */
+
 function validateAddress(){
 
-    let charAddress = document.getElementById("address").value.split(":").map(x => x);
-    let address = document.getElementById("address").value.split(":").map(x => parseInt(x, 16));
+    let charAddress = document.getElementById("address").value.split(":").map(x => x); // Uzima vrijednost iz elementa za unos adrese, vrijednosti između ':' sprema u polje kao znakovnu vrijednost
+    let address = document.getElementById("address").value.split(":").map(x => parseInt(x, 16)); // Uzima vrijednost iz elementa za unos adrese, vrijednosti između ':' sprema u polje kao cjelobrojnu vrijednost
 
     for(let i=0; i < charAddress.length; i++){
         for(let j=0; j < charAddress[i].length; j++){
 
-            let tmp=charAddress[i][j].toLowerCase();
+            let tmp=charAddress[i][j].toLowerCase(); // Varijabla j-tog znaka iz i-tog bloka u adresi
 
-            if(!((tmp>='a' && tmp<='f') || (tmp>='0' && tmp<='9'))){
+            if(!((tmp>='a' && tmp<='f') || (tmp>='0' && tmp<='9'))){ // Ispitivanje: je li znak u rasponu 'a' - 'f' ili '0' - '9'
                 return false;
             }
         }
     }
 
     for(let i = 0; i < address.length; i++){
-        if(!(address[i]>=0 && address[i]<=65535)){
+        if(!(address[i]>=0 && address[i]<=2**16)){ // Ispitivanje: je li znak u rasponu 0 - 2^16
             return false;
         }
     }
 
-    return true;
+    return true; // Ako su sve prethodne provjere uspješno izvedene, vraća 'true'
 }
+
+/**
+ * Za valjanu IPv6 adresu vrši skračivanje i ispisuje rezultat u HTML kodu
+ */
 
 function compressIPv6(){
 
-    if(validateAddress()){
+    if(validateAddress()){ // Provjerava je li upisna ispravna IPv6 adresa
 
+        // Uzima vrijednost iz elementa za unos adrese, vrijednosti između ':' sprema u polje kao cjelobrojnu vrijednost
         let address = document.getElementById("address").value.split(":").map(x => parseInt(x, 16));
 
-        let skipped = false;
-        let compressed = "";
+        let skipped = false; // Varijabla za provjeru preskočenog bloka nula
+        let compressed = ""; // Inicijalizacija stringa skraćene IPv6 adrese
 
-        for(let i = 0; i<address.length; i++){
-            if(address[i]!=0 || i===0){
+        for(let i = 0; i < address.length; i++){
+            if(address[i]==0 && skipped==false){ // Ako je vrijednost elementa polja jednaka 0 i nule već nisu preskočene
+                let start = i; // Sprema lokaciju prvog bloka nuli u adresi
+                skipped=true; 
+                for(i; i < address.length-1 && address[i+1]==0; i++); // for petlja koja prolazi kroz adresu i zaustavlja se kada element adrese bude različit od 0
+                compressed += (start > 0) ? ":" : "::"; // U string umeće ':' ili '::', ovisno o lokaciji prvog bloka nuli u adresi
+            }
+            else
+            {
+                // U string umeće vrijednost elementa adrese i ':', ovisno o lokaciji elementa
                 compressed += address[i].toString(16) + (i < 7 ? ":" : "");
             }
-            else if(skipped == false){
-                skipped = true;
-                for(i; i < address.length && address[i+1] === 0; i++);
-                compressed += ":";
-            }
-            else compressed += i < 7 ? "0:" : "0";
         }
 
+        // Ispis vrijednosti skrećene IPv6 adrese u elementu 'details' unutar HTML koda
         document.getElementById("details").innerHTML = "Skraćena IPv6 adresa: <strong>" + compressed + "</strong>";
-        document.getElementById("details").className = "active-details";
+        document.getElementById("details").className = "active-details"; // Primjena CSS-a na element 'details' umetanjem naziva klase
     }
-    else{
-        document.getElementById("details").innerHTML = "";
-        document.getElementById("details").className = "";
-        alert("Nije upisana ispravna IPv6 adresa!")
+    else
+    {
+        // Ako adresa nije ispravna
+        document.getElementById("details").innerHTML = ""; // Uklanjanje sadržaja unutar elementa 'details'
+        document.getElementById("details").className = ""; // Uklanjanje atribura klase za element 'details'
+        alert("Nije upisana ispravna IPv6 adresa!"); // Prikazivanje poruke o pogrešci unutar preglednika
     }
 }
 
+/**
+ * Postavlja vrijednosti elemenata na početne vrijednosti.
+ */
+
 function reset(){
-    document.getElementById("details").innerHTML = "";
-    document.getElementById("details").className = "";
-    document.getElementById("address").value = "";
+    document.getElementById("details").innerHTML = ""; // Uklanjanje sadržaja unutar elementa
+    document.getElementById("details").className = ""; // Uklanjanje atribura klase za element 'details'
+    document.getElementById("address").value = ""; // Brisanje sadržaja polja za unos IPv6 adrese
 }
